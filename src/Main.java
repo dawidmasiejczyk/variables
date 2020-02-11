728f6c78d4ed9ba559ef7b1d5d010c034608765c
dmas <dmas@wp.pl>
2020-02-11 15:51:30 +0100

    program with variables

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c;
+        c = a + b;
+        System.out.println(c);
+      //  System.out.println("Hello World!");
+    }
+}
